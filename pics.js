bfd0c47cdd8c70c889e2f136fffafd70250b9dc2
codesaurusrex-2001 <abbyrambarran@gmail.com>
2024-10-03 12:09:05 -0400

    fixed list template added logo added work in progress img

---
+++ pics.js
@@ -0,0 +1,23 @@
+const art = [
+    {
+        imgurl:"",
+    }
+]
+
+const ath = [
+    {
+        imgurl:"",
+    }
+]
+
+const bea = [
+    {
+        imgurl:"",
+    }
+]
+
+const bes = [
+    {
+        imgurl:"",
+    }
+]
